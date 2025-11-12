1a208a05e27ccebb7e3af0f232068046be23260c
Arnel P. Tagupa <arneltagupa1431@gmail.com>
2025-11-12 13:14:10 +0800

    replace the url logo with moovey logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,7 +6,7 @@ export default function AppLogo() {
             <img 
                 src="/images/moovey-logo.png" 
                 alt="Moovey Logo" 
-                className="h-16 w-auto sm:h-20 md:h-24 lg:h-28 transition-all duration-300 p-1"
+                className="h-20 w-auto sm:h-24 md:h-28 lg:h-32 xl:h-36 transition-all duration-300 p-1"
             />
         </>
     );
