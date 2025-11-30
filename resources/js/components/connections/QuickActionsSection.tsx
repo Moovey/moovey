20fd8c02bde59cc323b4bb41e24c37a3cabd5641
Arnel P. Tagupa <arneltagupa1431@gmail.com>
2025-11-30 15:58:34 +0800

    QuickActionsSection is now fully functional

--- resources/js/components/connections/QuickActionsSection.tsx
+++ resources/js/components/connections/QuickActionsSection.tsx
@@ -29,12 +29,6 @@ export default function QuickActionsSection() {
                         </svg>
                         Join Community
                     </Link>
-                    <button className="w-full bg-gray-100 text-gray-700 p-3 rounded-lg text-sm font-medium hover:bg-gray-200 transition-colors flex items-center justify-center">
-                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                        </svg>
-                        Send Referral
-                    </button>
                 </div>
             </div>
         </div>
